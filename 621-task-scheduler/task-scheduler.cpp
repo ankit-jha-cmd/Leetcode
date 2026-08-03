ecda8cef0c9d7a05ec357ae1eaaabac6a0bce69c
Ankit <ankitkumar400100@gmail.com>
2026-08-03 18:24:43 +0530

    Time: 16 ms (54.55%) | Memory: 43.1 MB (47.61%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        if(n==0) return tasks.size();
         unordered_map<char, int>freq;
-        unordered_map<char, int>free_;
-        for(int i=0;i<tasks.size();i++){
-            freq[tasks[i]]++;
-            free_[tasks[i]]=1;
-        }
-        int slot=1;
-        vector<pair<int, char>>cooldown;
-        priority_queue<pair<int, char>>pq;
-        for(auto it: freq) pq.push({it.second, it.first});
+        for(int i=0;i<tasks.size();i++) freq[tasks[i]]++;
+        priority_queue<int>pq;
+        for(auto it: freq) pq.push(it.second);
+        queue<pair<int, int>>cooldown;
+        int slot=0;
         while(!pq.empty() || !cooldown.empty()){
-            vector<pair<int, char>>stillWaiting;
-                for(int i=0;i<cooldown.size();i++){
-                    if(slot>= free_[cooldown[i].second]) pq.push(cooldown[i]);
-                    else stillWaiting.push_back(cooldown[i]);
-                }
-                cooldown=stillWaiting;
-                if(pq.empty()){
-                    slot++;
-                    continue;
-                }
-                auto el=pq.top();
+            slot++;
+            if(!pq.empty()){
+                int occ=pq.top();
                 pq.pop();
-                char val=el.second;
-                int occ=el.first-1;
-                free_[val]=slot+n+1;
-                if(occ>=1) cooldown.push_back({occ, val});
-                slot++;
+                occ--;
+                if(occ>0) cooldown.push({occ, slot+n});
             }
-        return slot-1;
+            if(!cooldown.empty() && cooldown.front().second==slot){
+                pq.push(cooldown.front().first);
+                cooldown.pop();
+            }
+        }
+        return slot;
     }
 };
