e9d679e14c047862a1bcdffda61dedeace9aa51c
Ankit <ankitkumar400100@gmail.com>
2026-08-27 20:35:11 +0530

    Time: 15 ms (9.84%) | Memory: 20.3 MB (16.98%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+bool check(vector<vector<int>>&adj, int node, vector<int>&vis, vector<int>&path){
+      vis[node]=1;
+      path[node]=1;
+      for(auto it: adj[node]){
+          if(!vis[it]){
+              if(!check(adj, it, vis, path)) return false;
+          }
+          else if(path[it]) return false;
+          
+      }
+      path[node]=0;
+      return true;
+  }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(numCourses);
+        for(auto it: prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int>vis(numCourses, 0);
+        vector<int>path(numCourses, 0);
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){
+             if(!check(adj, i, vis, path)) return false;   
+            }
+        }
+        return true;
+    }
+};
