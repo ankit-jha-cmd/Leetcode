ec864a839bbf07133a1d4bfd182720916aee7dd3
Ankit <ankitkumar400100@gmail.com>
2026-09-06 21:24:35 +0530

    Time: 17 ms (77.57%) | Memory: 89.7 MB (7.57%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-vector<int>psefn(vector<int>& arr){
-    int n=arr.size();
-        vector<int>ans(n, -1);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[st.top()]>arr[i]){
-                ans[st.top()]=i;
-                st.pop();
-            }
-            st.push(i);
+vector<int> findpse(vector<int>& arr, int n){
+    stack<int>st;
+    vector<int>ans(n,-1);
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && arr[i]<arr[st.top()]){
+            ans[st.top()]=i;
+            st.pop();
         }
-        return ans;
+        st.push(i);
+    }
+    return ans;
 }
-vector<int>nsefn(vector<int>& arr){
-    int n=arr.size();
-     stack<int>st;
-        vector<int>ans(n, arr.size());
-        for(int i=0;i<n;i++){
-                while(!st.empty() && arr[i]<arr[st.top()]){
-                    ans[st.top()]=i;
-                    st.pop();
-                }    
-                st.push(i);
+vector<int> findnse(vector<int>& arr, int n){
+    stack<int>st;
+    vector<int>ans(n,n);
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[i]<arr[st.top()]){
+            ans[st.top()]=i;
+            st.pop();
         }
-        return ans;
+        st.push(i);
+    }
+    return ans;
 }
     int largestRectangleArea(vector<int>& heights) {
-        int maxi=0;
-        vector<int>nse=nsefn(heights);
-        vector<int>pse = psefn(heights);
-        for(int i=0;i<heights.size();i++){
-            maxi=max(maxi, heights[i]*(nse[i]-pse[i]-1));
+        int n=heights.size();
+        vector<int>nse=findnse(heights, n);
+        vector<int>pse=findpse(heights, n);
+        int maxarea=0;
+        for(int i=0;i<n;i++){
+           maxarea=max(maxarea, heights[i]*(nse[i]-pse[i]-1)); 
         }
-        return maxi;
+        return maxarea;
     }
 };
