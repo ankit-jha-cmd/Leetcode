0bd3c9c8f1d4e6a9a260cd815153b080d99452fe
Ankit <ankitkumar400100@gmail.com>
2026-09-18 05:16:40 +0530

    Time: 7 ms (60.73%) | Memory: 11.6 MB (43.79%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int l=0;
-        int r=0;
-        int cnt=0;
+        int n=s.size();
+        int m=t.size();
+        if(n<m)return "";
+        string ans="";
         int minlen=INT_MAX;
+        int i=0;
+        int j=0;
+        int cnt=0;
         int start=0;
-        if(s.size()<t.size()) return "";
-        vector<int>freq(128,0);
-        for(char c: t) freq[c]++;
-        while(r<s.size()){
-            if(freq[s[r]]>0) cnt++;
-            freq[s[r]]--;
-            while(cnt == t.size()){
-                if(r-l+1<minlen){
-                    minlen=r-l+1;
-                    start=l;
+        unordered_map<char, int>mp;
+        for(int i=0;i<m;i++) mp[t[i]]++;
+        while(j<n){
+            if(mp[s[j]]>0) cnt++;
+            mp[s[j]]--;
+            while(cnt==m){
+                if((j-i+1)<minlen){
+                    minlen=j-i+1;
+                    start=i;
                 }
-                freq[s[l]]++;
-                if(freq[s[l]]>0) cnt--;
-                l++;
+                mp[s[i]]++;
+                if(mp[s[i]]>0) cnt--;
+                i++;
             }
-            r++;
+            j++;
         }
-        return minlen==INT_MAX? "" : s.substr(start, minlen);
+        return minlen==INT_MAX ? "" : s.substr(start, minlen);
     }
 };
